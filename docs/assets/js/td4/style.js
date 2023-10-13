a8ce140cb5f6418b38d37d9c271df61635f6433e
Makrotoo <144342901+SpongesBoob@users.noreply.github.com>
2023-10-13 14:16:45 +0200

    TD4 evolution

--- docs/assets/js/td4/style.js
+++ docs/assets/js/td4/style.js
@@ -49,3 +49,95 @@ function clearResult() {
   currentOperation = '';
   resultField.value = '';
 }
+// Récupérer la liste de tâches depuis le local storage
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const saveNameInput = document.getElementById('saveName');
+const loadSelect = document.getElementById('loadSelect');
+
+// Afficher la liste de tâches
+function displayTasks() {
+  taskList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox" onchange="toggleTask(${index})" ${task.completed ? 'checked' : ''}>
+      <span class="${task.completed ? 'completed' : ''}">${task.name}</span>
+      <button onclick="editTask(${index})">Modifier</button>
+      <button onclick="deleteTask(${index})">Supprimer</button>
+    `;
+    taskList.appendChild(li);
+  });
+}
+
+// Ajouter une nouvelle tâche
+function addTask() {
+  const newTask = { name: taskInput.value, completed: false };
+  tasks.push(newTask);
+  taskInput.value = '';
+  saveTasks();
+  displayTasks();
+}
+
+// Cocher/décocher une tâche
+function toggleTask(index) {
+  tasks[index].completed = !tasks[index].completed;
+  saveTasks();
+  displayTasks();
+}
+
+// Modifier une tâche
+function editTask(index) {
+  const newName = prompt('Entrez le nouveau nom de la tâche :');
+  tasks[index].name = newName;
+  saveTasks();
+  displayTasks();
+}
+
+// Supprimer une tâche
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks();
+  displayTasks();
+}
+
+// Sauvegarder les tâches dans le local storage avec un nom choisi
+function saveTasks() {
+  const saveName = saveNameInput.value;
+  if (saveName !== '') {
+    localStorage.setItem(saveName, JSON.stringify(tasks));
+    saveNameInput.value = '';
+    updateLoadSelect();
+  }
+}
+
+// Charger les tâches sauvegardées
+function loadTasks() {
+  const selectedSave = loadSelect.value;
+  const savedTasks = JSON.parse(localStorage.getItem(selectedSave));
+  if (savedTasks) {
+    tasks = savedTasks;
+    displayTasks();
+  }
+}
+
+// Mettre à jour la liste des sauvegardes disponibles
+function updateLoadSelect() {
+  loadSelect.innerHTML = '<option value="">Choisir une sauvegarde</option>';
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key !== 'tasks') {
+      const option = document.createElement('option');
+      option.value = key;
+      option.textContent = key;
+      loadSelect.appendChild(option);
+    }
+  }
+}
+
+// Charger les tâches sauvegardées lors du chargement de la page
+updateLoadSelect();
+displayTasks();
